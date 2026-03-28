c48f04f02997ee2bd796ee1e82a8ff1505fe54a0
Shweta <shweta@bentraytech.com>
2026-03-28 19:33:21 +0545

    Generating report(pdf,word,excel) of employee

---
+++ EmployeeInformationSystem/EmployeeInformationSystem/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeInformationSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Department { get; set; }
+        public double Salary { get; set; }
+    }
+}
